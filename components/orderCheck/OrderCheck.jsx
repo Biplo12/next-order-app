d0a82956eb6bde9d659ee4c8715b53ca4acd4adb
Biplo12 <robertbiplosek@gmail.com>
2022-08-17 13:15:19 +0200

    feat: order checker

--- components/orderCheck/OrderCheck.jsx
+++ components/orderCheck/OrderCheck.jsx
@@ -1,19 +1,40 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./OrderCheck.module.scss";
 const OrdersCheck = ({ ordersList }) => {
   const [orderId, setOrderId] = useState("");
   const [order, setOrder] = useState("");
+  const [value, setValue] = useState("");
   const [error, setError] = useState(false);
-  const handleCheck = () => {
+
+  let myRef = useRef();
+
+  const handleUserInput = (e) => {
+    e.preventDefault();
+    setOrderId(e.target.value);
+    setValue(e.target.value);
+  };
+
+  const handleCheck = (e) => {
+    e.preventDefault();
     const filteredOrder = ordersList.filter((id) => id._id === orderId);
     setOrder(filteredOrder);
-    filteredOrder.length === 0 ? setError(true) : "";
+    setError(filteredOrder.length === 0 ? true : false);
   };
-  const handleReset = () => {
+
+  const handleReset = (e) => {
+    e.preventDefault();
     setOrder("");
+    setValue("");
     setOrderId("");
     setError(false);
   };
+
+  useEffect(() => {
+    if (order.length != 0) {
+      myRef.current.scrollIntoView();
+    }
+  }, [order]);
+
   return (
     <>
       <div className={styles.container}>
@@ -27,22 +48,45 @@ const OrdersCheck = ({ ordersList }) => {
           </p>
         </div>
         <div className={styles.bottom}>
-          <input
-            placeholder={"Identyfikator zlecenia"}
-            type="text"
-            onChange={(e) => setOrderId(e.target.value)}
-          />
-          <div className={styles.buttons}>
-            <button onClick={handleCheck}>Sprawdź status</button>
-            <button type="reset" onClick={handleReset}>
-              Zresetuj
-            </button>
-          </div>
-          {error && <h1>Nieprawidłowy identyfikator zlecenia</h1>}
+          <form onSubmit={handleCheck}>
+            <input
+              placeholder={"Identyfikator zlecenia"}
+              type="text"
+              value={value}
+              onChange={handleUserInput}
+            />
+            <div className={styles.buttons}>
+              <button type="submit" onClick={handleCheck}>
+                Sprawdź status
+              </button>
+              <button type="reset" onClick={handleReset}>
+                Zresetuj
+              </button>
+            </div>
+          </form>
+          {!order && (
+            <div className={styles.example}>
+              <p>Przykładowy identyfikator: 62ec06988c8f19392f0e047e</p>
+            </div>
+          )}
+          {error && (
+            <>
+              <h4 className={styles.error}>
+                Nieprawidłowy identyfikator zlecenia
+              </h4>
+              <div className={styles.example}>
+                <p>Przykładowy identyfikator: 62ec06988c8f19392f0e047e</p>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {order && (
-        <div className={order.length === 0 ? "" : styles.orderDetails}>
+        <div
+          ref={myRef}
+          className={order.length === 0 ? "" : styles.orderDetails}
+        >
+          <h1>{order[0]?.status}</h1>
           <h2>{order[0]?.title}</h2>
           <p>{order[0]?.desc}</p>
         </div>
